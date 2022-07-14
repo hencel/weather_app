7351279d68a242b6f12e3fe7c519d1f28eaa5039
Zbigniew Piernik <zbigniewpiernik@gmail.com>
2022-07-14 14:50:03 +0200

    003 Added models interfaces to data from Api

---
+++ src/app/shared/model/City.ts
@@ -0,0 +1,12 @@
+import { Coord } from "./Coord";
+
+export class City {
+    name: string;
+    country: string;
+    id: number;
+    population: number;
+    sunrise: number;
+    sunset: number;
+    timezone: number;
+    coord: Coord;
+}
---
+++ src/app/shared/model/Main.ts
@@ -0,0 +1,10 @@
+export class Main {
+    feels_like: number;
+    grnd_level: number;
+    humidity: number;
+    pressure: number;
+    sea_level: number;
+    temp_kf: number;
+    temp_max: number;
+    temp_min: number;
+}
